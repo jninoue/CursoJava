660c18d2dd37f1c2985e718d802a0268f486eb4e
usuario <usuario@HOME_PC>
2022-07-17 14:57:59 -0300

    Criado classe Televisao e programa exercício 04

---
+++ src/br/com/geekuniversity/secao11/Exercicio04.java
@@ -0,0 +1,14 @@
+package br.com.geekuniversity.secao11;
+
+public class Exercicio04 {
+	public static void main(String[] args) {
+		Televisao tv1 = new Televisao();
+		ControleRemoto cr1 = new ControleRemoto(tv1);
+		
+		cr1.aumenta_volume();
+		System.out.println("Volume televisao é: " + tv1.getVolume());
+	}
+	
+	
+	
+}
---
+++ src/br/com/geekuniversity/secao11/Televisao.java
@@ -0,0 +1,13 @@
+package br.com.geekuniversity.secao11;
+
+public class Televisao {
+	int volume, canal;
+	
+	public int getVolume() {
+		return volume;
+	}
+	
+	public int getCanal() {
+		return canal;
+	}
+}
