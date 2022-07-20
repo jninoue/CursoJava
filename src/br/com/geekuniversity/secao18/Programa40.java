d8c741d7dbd8de546cf779c3109db231cb1e2fd9
usuario <usuario@HOME_PC>
2022-07-20 07:18:16 -0300

    biblioteca Math

---
+++ src/br/com/geekuniversity/secao18/Programa40.java
@@ -0,0 +1,20 @@
+package br.com.geekuniversity.secao18;
+
+public class Programa40 {
+
+	public static void main(String[] args) {
+		System.out.println(Math.PI);
+		
+		System.out.println(Math.round(Math.PI));
+		
+		System.out.println(Math.ceil(Math.PI));
+		
+		System.out.println(Math.floor(Math.PI));
+		
+		System.out.println(Math.pow(3,2));
+		
+		System.out.println(Math.sqrt(9)); // raiz quadrada
+
+	}
+
+}
