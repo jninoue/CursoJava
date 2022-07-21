0b6963984192a3fa085c258208981790b4bf75af
usuario <usuario@HOME_PC>
2022-07-21 20:14:09 -0300

    Aula sobre Lista

--- src/br/com/geekuniversity/secao11/Cliente.java
+++ src/br/com/geekuniversity/secao11/Cliente.java
@@ -1,9 +1,11 @@
 package br.com.geekuniversity.secao11;
 
-public class Cliente {
+public class Cliente implements Comparable{
 	String nome, endereco;
+	int idade;
 	
-	public Cliente(String nome, String endereco) {
+	public Cliente(int idade, String nome, String endereco) {
+		this.idade = idade;
 		this.nome = nome;
 		this.endereco = endereco;
 	}
@@ -14,5 +16,22 @@ public class Cliente {
 	
 	public String getEndereco() {
 		return endereco;
+	}
+	
+	public String toString() {
+		return this.nome;
+	}
+
+	@Override
+	public int compareTo(Object outro) {
+		Cliente aux = (Cliente)outro;
+		if (this.idade < aux.idade) {
+			return -1;
+		}else if(this.idade > aux.idade) {
+			return 1;
+		}else {
+			return 0;	
+		}
+		
 	}
 }
---
+++ src/br/com/geekuniversity/secao19/Programa45.java
@@ -0,0 +1,39 @@
+package br.com.geekuniversity.secao19;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Programa45 {
+
+	public static void main(String[] args) {
+		
+		//ArryList nomes = new ArrayList(); // sem definir o tipo de dado, podemos colocar qualquer tipo.
+		List<String> nomes = new ArrayList<String>();
+		
+		nomes.add("Maria");
+		nomes.add("Felicity");
+		nomes.add("Angelina");
+		nomes.add("Geek");
+		nomes.add("Maria");
+		
+//		nomes.add(44);
+//		nomes.add(true);
+//		nomes.add(14.6);
+//		nomes.add('e');
+
+//		System.out.println(nomes.get(0));
+//		System.out.println(nomes.get(3));
+		
+//		for (int i = 0; i < nomes.size(); i++) {
+//			System.out.println(nomes.get(i));
+//		}
+		
+		Collections.sort(nomes);
+		
+		for (Object nome : nomes){
+			System.out.println(nome);
+		}
+	}
+
+}
