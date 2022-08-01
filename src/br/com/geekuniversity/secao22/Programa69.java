d1e010059e66735007f976fca6d7908062cf9a46
usuario <usuario@HOME_PC>
2022-08-01 15:01:57 -0300

    Classes anonimas

---
+++ src/br/com/geekuniversity/secao22/Programa69.java
@@ -0,0 +1,38 @@
+package br.com.geekuniversity.secao22;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Programa69 {
+
+	public static void main(String[] args) {
+		List<String> palavras = new ArrayList<String>();
+		
+		Comparator<String> comparador = new ComparadorPorTamanho();
+		
+		palavras.add("Geek");
+		palavras.add("Unisersity");
+		palavras.add("Java");
+		palavras.add("Angelina");
+		palavras.add("Playstation 4");
+		palavras.add("Outlast 2");
+		
+		palavras.sort(comparador);
+		
+		//classe anonima
+		Consumer<String> consumidor = new Consumer<String>() {
+
+			@Override
+			public void accept(String s) {
+				System.out.println(s);
+				
+			}
+			
+		};
+		palavras.forEach(consumidor);
+
+	}
+
+}
